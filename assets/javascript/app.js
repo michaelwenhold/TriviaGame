8f6402162532aa27c43670edd3a894ec7df683fe
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-08-29 22:16:52 -0500

    TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+//Questions, Choices, & Answers
+questions: {
+    q1: "What company's slogan should be: Connect with people for no reason at all";
+    q2: "What restaurant's slogan should be: It's 2am and you're drunk";
+    q3: "What product's slogan should be: Every bite is a different temperature";
+    q4: "What product's slogan should be: Slip into a nice coma for a few hours";
+    q5: "What product's slogan should be: You have exactly one minute to enjoy this cereal";
+    q6: "What product's slogan should be: How many days straight can I eat this before dying";
+    q7: "What company's slogan should be: We have a salad bar for some reason";
+    q8: "What product's slogan should be: Be careful not to poke a hole through the back";
+    q9: "What company's slogan should be: Pay money to look homeless";
+    q10: "What product's slogan should be: They are all the same flavor";
+    q11: "What company's slogan should be: We'll leave the light on for you because you're in a sketchy neighborhood";
+    q12: "What product's slogan should be: All the vegetables you'll ever need if you could stop mixing it with vodka";
+};
+
+choices: {
+    q1: ["Twitter", "LinkedIn", "Snapchat", "Tinder"];
+    q2: ["Hopdoddy", "Home Slice", "Waffle House", "Chick-fil-a"];
+    q3: ["Hot Pockets", "Wheat Thins", "Go-Gurt", "Gusters"];
+    q4: ["Rogaine", "Axe", "NyQuil", "Preparation H"];
+    q5: ["Grape Nuts", "Lucky Charms", "Raisin Bran", "Rice Krispies"];
+    q6: ["Ramen Noodles", "Spaghetti O's", "Kraft Mac & Cheese", "Slim Jims"];
+    q7: ["Jason's Deli", "Subway", "Taco Bell", "Pizza Hut"];
+    q8: ["Trojan", "Nike", "Capri Sun", "Life Saver"];
+    q9: ["Urban Outfitters", "Levi's", "Kate Spade", "Yeti"];
+    q10: ["Skittles", "Jelly Belly", "Fruit Loops", "Sour Patch Kids"];
+    q11: ["Hilton", "Motel 6", "Holiday Inn", "Anything on Airbnb"];
+    q12: ["Sprite", "Cranberry Juice", "V8", "Orange Juice"];
+};
+
+answers: {
+    q1: "LinkedIn";
+    q2: "Waffle House";
+    q3: "Hot Pockets";
+    q4: "NyQuil";
+    q5: "Rice Krispies";
+    q6: "Ramen Noodles";
+    q7: "Pizza Hut";
+    q8: "Capri Sun";
+    q9: "Urban Outfitters";
+    q10: "Fruit Loops";
+    q11: "Motel 6";
+    q12: "V8";
+};
