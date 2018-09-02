288008e9b2f57407d97154e696c9f335cd550afe
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-09-02 15:52:14 -0500

    Trivia Game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,98 @@
-//Questions, Choices, & Answers
-questions: {
-    q1: "What company's slogan should be: Connect with people for no reason at all";
-    q2: "What restaurant's slogan should be: It's 2am and you're drunk";
-    q3: "What product's slogan should be: Every bite is a different temperature";
-    q4: "What product's slogan should be: Slip into a nice coma for a few hours";
-    q5: "What product's slogan should be: You have exactly one minute to enjoy this cereal";
-    q6: "What product's slogan should be: How many days straight can I eat this before dying";
-    q7: "What company's slogan should be: We have a salad bar for some reason";
-    q8: "What product's slogan should be: Be careful not to poke a hole through the back";
-    q9: "What company's slogan should be: Pay money to look homeless";
-    q10: "What product's slogan should be: They are all the same flavor";
-    q11: "What company's slogan should be: We'll leave the light on for you because you're in a sketchy neighborhood";
-    q12: "What product's slogan should be: All the vegetables you'll ever need if you could stop mixing it with vodka";
+//Question Variables
+
+let question0 = {
+    question: "What company's slogan should be: Connect with people for no reason at all",
+    answer: "LinkedIn",
+    choices: ["Twitter", "LinkedIn", "Snapchat", "Tinder"],
+    correctAnswer: 1,
+    correctImage: "assets/images/image0.png"
 };
 
-choices: {
-    q1: ["Twitter", "LinkedIn", "Snapchat", "Tinder"];
-    q2: ["Hopdoddy", "Home Slice", "Waffle House", "Chick-fil-a"];
-    q3: ["Hot Pockets", "Wheat Thins", "Go-Gurt", "Gusters"];
-    q4: ["Rogaine", "Axe", "NyQuil", "Preparation H"];
-    q5: ["Grape Nuts", "Lucky Charms", "Raisin Bran", "Rice Krispies"];
-    q6: ["Ramen Noodles", "Spaghetti O's", "Kraft Mac & Cheese", "Slim Jims"];
-    q7: ["Jason's Deli", "Subway", "Taco Bell", "Pizza Hut"];
-    q8: ["Trojan", "Nike", "Capri Sun", "Life Saver"];
-    q9: ["Urban Outfitters", "Levi's", "Kate Spade", "Yeti"];
-    q10: ["Skittles", "Jelly Belly", "Fruit Loops", "Sour Patch Kids"];
-    q11: ["Hilton", "Motel 6", "Holiday Inn", "Anything on Airbnb"];
-    q12: ["Sprite", "Cranberry Juice", "V8", "Orange Juice"];
+let question1 = {
+    question: "What restaurant's slogan should be: It's 2am and you're drunk",
+    answer: "Waffle House",
+    choices: ["Hopdoddy", "Home Slice", "Waffle House", "Chick-fil-a"],
+    correctAnswer: 2,
+    correctImage: "assets/images/image1.jpg"
 };
 
-answers: {
-    q1: "LinkedIn";
-    q2: "Waffle House";
-    q3: "Hot Pockets";
-    q4: "NyQuil";
-    q5: "Rice Krispies";
-    q6: "Ramen Noodles";
-    q7: "Pizza Hut";
-    q8: "Capri Sun";
-    q9: "Urban Outfitters";
-    q10: "Fruit Loops";
-    q11: "Motel 6";
-    q12: "V8";
+let question2 = {
+    question: "What product's slogan should be: Every bite is a different temperature",
+    answer: "Hot Pockets",
+    choices: ["Hot Pockets", "Wheat Thins", "Go-Gurt", "Gusters"],
+    correctAnswer: 0,
+    correctImage: "assets/images/image2.jpg"
 };
+
+let question3 = {
+    question: "What product's slogan should be: Slip into a nice coma for a few hours",
+    answer: "NyQuil",
+    choices: ["Rogaine", "Axe", "NyQuil", "Preparation H"],
+    correctAnswer: 2,
+    correctImage: "assets/images/image3.jpg"
+};
+
+let question4 = {
+    question: "What product's slogan should be: You have exactly one minute to enjoy this cereal",
+    answer: "Rice Krispies",
+    choices: ["Grape Nuts", "Lucky Charms", "Raisin Bran", "Rice Krispies"],
+    correctAnswer: 3,
+    correctImage: "assets/images/image4.jpg"
+};
+
+let question5 = {
+    question: "What product's slogan should be: How many days straight can I eat this before dying",
+    answer: "Ramen Noodles",
+    choices: ["Ramen Noodles", "Spaghetti O's", "Kraft Mac & Cheese", "Slim Jims"],
+    correctAnswer: 0,
+    correctImage: "assets/images/image5.jpg"
+};
+
+let question6 = {
+    question: "What company's slogan should be: We have a salad bar for some reason",
+    answer: "Pizza Hit",
+    choices: ["Jason's Deli", "Subway", "Taco Bell", "Pizza Hut"],
+    correctAnswer: 3,
+    correctImage: "assets/images/image6.jpg"
+};
+
+let question7 = {
+    question: "What product's slogan should be: Be careful not to poke a hole through the back",
+    answer: "Capri Sun",
+    choices: ["Trojan", "Nike", "Capri Sun", "Life Saver"],
+    correctAnswer: 2,
+    correctImage: "assets/images/image7.jpg"
+};
+
+let question8 = {
+    question: "What company's slogan should be: Pay money to look homeless",
+    answer: "Urban Outfitters",
+    choices: ["Urban Outfitters", "Levi's", "Kate Spade", "Yeti"],
+    correctAnswer: 0,
+    correctImage: "assets/images/image8.png"
+};
+
+let question9 = {
+    question: "What product's slogan should be: They are all the same flavor",
+    answer: "Fruit Loops",
+    choices: ["Skittles", "Jelly Belly", "Fruit Loops", "Sour Patch Kids"],
+    correctAnswer: 2,
+    correctImage: "assets/images/image9.jpg"
+};
+
+let question10 = {
+    question: "What company's slogan should be: We'll leave the light on for you because you're in a sketchy neighborhood",
+    Answer: "Motel 6",
+    choices: ["Hilton", "Motel 6", "Holiday Inn", "Airbnb"],
+    correctAnswer: 1,
+    correctImage: "assets/images/image10.jpg"
+};
+
+let question11 = {
+    question: "What product's slogan should be: All the vegetables you'll ever need if you could stop mixing it with vodka",
+    answer: "V8",
+    choices: ["Sprite", "Cranberry Juice", "V8", "Orange Juice"],
+    correctAnswer: 2,
+    correctImage: "assets/images/image11.jpg"
+};
+
